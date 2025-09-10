ca25d9053169ce48e8c0b7da0ce43d053295df2b
ankit <sahuankit.murhu@gmail.com>
2025-09-10 13:09:25 +0530

    feat(native): root ui done

---
+++ apps/native/app/index.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import colors from '../styles/colors';
+import { Link } from 'expo-router';
+import RText from '../components/RText';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <RText style={styles.hero}>Tourbuddy 👋</RText>
+      <RText style={styles.text}>ur buddy our buddy tour buddy</RText>
+      <Link href={'/dashboard/map'} asChild>
+        <TouchableOpacity style={styles.button}>
+          <RText style={styles.buttonText}>Goto Map</RText>
+        </TouchableOpacity>
+      </Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 5,
+  },
+  hero: {
+    color: colors['zinc-100'],
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: colors['zinc-500'],
+    fontSize: 15,
+  },
+  buttonText :{
+    color: colors['zinc-100'],
+  },
+  button: {
+    backgroundColor: colors['zinc-800'],
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  }
+});
