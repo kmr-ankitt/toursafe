189925e1bccc119df7489c0a0ee67e8490c36c18
[Alka Mishra] <[alkam7974@gmail.com]>
2025-09-11 01:13:45 +0530

    feat(db): prisma schema added

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,33 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import prisma from './client'; 
 
-const app = express()
+dotenv.config(); 
+
+const app = express();
+app.use(express.json());
+
+
+async function testDBConnection() {
+  try {
+    await prisma.$connect(); 
+    console.log(' Database connected successfully!');
+  } catch (error) {
+    console.error(' Database connection failed:', error);
+  }
+}
+
+
+testDBConnection();
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+  res.send('Hello, World!');
+});
 
-app.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000')
-})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
