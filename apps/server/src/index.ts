05cbc55d8fe5c3e07f2a973da73f5d5ca2f1983a
ankit <sahuankit.murhu@gmail.com>
2025-09-12 01:10:13 +0530

    fix(server): cors error fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import prisma from './prismaClient/client';
 import touristRouter from './routes/touristRoutes';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
